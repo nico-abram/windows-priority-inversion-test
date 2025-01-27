b18fd93cfbb619f0f59274fecc0282e79936a1a2
unknown <abramlujan@gmail.com>
2025-01-27 19:15:16 -0300

    C file

---
+++ priority_inversion.c
@@ -0,0 +1,94 @@
+
+#include <windows.h>
+#include <synchapi.h>
+#include <processthreadsapi.h>
+
+volatile ULONG LOW_PRIO_THREAD_READY = 0;
+volatile ULONG LAUNCH_LOW_PRIO = 0;
+volatile ULONG LAUNCH_MED_PRIO = 0;
+volatile ULONG LAUNCH_HI_PRIO = 0;
+volatile ULONG HI_PRIO_FINISHED = 0;
+volatile ULONG SYNC_INT = 0;
+HANDLE MUTEX;
+volatile ULONG64 UNREAD_VOLATILE = 0;
+volatile ULONG64 UNREAD_VOLATILE2 = 0;
+
+void wait(ULONG* var) {
+	UINT local_sync_value = *var;
+	while (local_sync_value == 0) {
+	      static ULONG zero = 0;
+	      WaitOnAddress(var, &zero, sizeof(ULONG), INFINITE);
+	      local_sync_value = *var;
+	}
+}
+void set(ULONG* var) {
+	*var = 1;
+	WakeByAddressAll(var);
+}
+void low_prio_thread() {
+	// Lock the mutex ASAP
+	WaitForSingleObject(MUTEX, INFINITE);
+
+	set(&LOW_PRIO_THREAD_READY);
+
+	wait(&LAUNCH_LOW_PRIO);
+	
+
+	while (UNREAD_VOLATILE2 != 0x1FFFFFF) {
+		UNREAD_VOLATILE2++;
+	}
+	
+	ReleaseMutex(MUTEX);
+}
+void med_prio_thread() {
+	wait(&LAUNCH_MED_PRIO);
+
+	set(&LAUNCH_LOW_PRIO);
+
+	// Starve the cpu
+	while (UNREAD_VOLATILE != 0xFFFFFFFFFFFFFFFE) {
+		UNREAD_VOLATILE++;
+	}
+}
+void hi_prio_thread() {
+	wait(&LAUNCH_HI_PRIO);
+
+	set(&LAUNCH_MED_PRIO);
+	
+	// Lock mutex held by low priority thread
+	WaitForSingleObject(MUTEX, INFINITE);
+
+	set(&HI_PRIO_FINISHED);
+}
+
+int main() {
+
+	// Starve the cpu
+	while (UNREAD_VOLATILE != 0xFFFFFFFFFFFFFFFE) {
+		UNREAD_VOLATILE++;
+	}
+	MUTEX = CreateMutex(0, 0, 0);
+
+	HANDLE low_handle = CreateThread(0, 1024*100, low_prio_thread, 0, 0, 0);
+	HANDLE med_handle = CreateThread(0, 1024*100, med_prio_thread, 0, 0, 0);
+	HANDLE hi_handle = CreateThread(0, 1024*100, hi_prio_thread, 0, 0, 0);
+	
+	SetThreadPriority(low_handle, THREAD_PRIORITY_NORMAL);
+	SetThreadPriority(med_handle, THREAD_PRIORITY_ABOVE_NORMAL);
+	// SetThreadPriority(hi_handle, THREAD_PRIORITY_HIGHEST);
+
+	SetThreadPriority(hi_handle, THREAD_PRIORITY_NORMAL);
+
+	// Force them to run on the same core
+	SetThreadAffinityMask(low_handle, 0x4);
+	SetThreadAffinityMask(med_handle, 0x4);
+	SetThreadAffinityMask(hi_handle, 0x4);
+
+	wait(&LOW_PRIO_THREAD_READY);
+
+	set(&LAUNCH_HI_PRIO);
+
+	wait(&HI_PRIO_FINISHED);
+
+	return 0;
+}
